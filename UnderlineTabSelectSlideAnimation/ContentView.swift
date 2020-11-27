2149e8739edb1b8c976ea7ebf96791e8f7ee5844
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-27 08:55:27 +0200

    Initial Commit

---
+++ UnderlineTabSelectSlideAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnderlineTabSelectSlideAnimation
+//
+//  Created by Ramill Ibragimov on 27.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnderlineTabSelectSlideAnimation/UnderlineTabSelectSlideAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderlineTabSelectSlideAnimationApp.swift
+//  UnderlineTabSelectSlideAnimation
+//
+//  Created by Ramill Ibragimov on 27.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct UnderlineTabSelectSlideAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
