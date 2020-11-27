1040fdf6c2d4b36e812ef3ff8f7fe64e5b57da2f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-27 09:13:14 +0200

    init

--- UnderlineTabSelectSlideAnimation/ContentView.swift
+++ UnderlineTabSelectSlideAnimation/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let categories = ["Fruits", "Fish", "Dairy"]
+    @State private var selectedCategory = "Fruits"
+    @Namespace private var animation
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
+                    ForEach(categories, id: \.self) { category in
+                        Button(action: {
+                            withAnimation {
+                                selectedCategory = category
+                            }
+                        }
+                        , label: {
+                            VStack(spacing: 4) {
+                                Text(category)
+                                    .font(.system(size: 18))
+                                    .fontWeight(.semibold)
+                                if selectedCategory == category {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .frame(width: 40, height: 2)
+                                        .matchedGeometryEffect(id: "category", in: animation)
+                                }
+                            }
+                        })
+                        .accentColor(.primary)
+                    }
+                }
+            }
+            Spacer()
+        }
+        .navigationBarTitle("Groceries")
+        .padding()
     }
 }
 
